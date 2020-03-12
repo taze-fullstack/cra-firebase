ea45d6067fba6a82b6f2fb5ab78ed49b82363bc3
taze-fullstack <taze@fullstack.ph>
2020-03-12 17:16:45 +0800

    add initial CR[U]D for items

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,110 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
-function App() {
+import firebase from '../firebase';
+
+const App = () => {
+  const [name, setName] = useState('');
+  const [color, setColor] = useState('');
+  const [items, setItems] = useState();
+
+  useEffect(
+    () => {
+      const itemsRef = firebase.database().ref('items');
+
+      itemsRef.on('value', (snapshot) => {
+        let items = snapshot.val();
+        let newState = [];
+        for (let item in items) {
+          newState.push({
+            id: item,
+            name: items[item].name,
+            color: items[item].color,
+          });
+        }
+        setItems(newState);
+      });
+    },
+    [
+      /* Trigger once */
+    ]
+  );
+
+  const handleName = (e) => {
+    setName(e.currentTarget.value);
+  };
+
+  const handleColor = (e) => {
+    setColor(e.currentTarget.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const itemsRef = firebase.database().ref('items');
+    const item = {
+      name: name,
+      color: color,
+    };
+
+    itemsRef.push(item);
+    setName('');
+    setColor('');
+  };
+
+  const removeItem = (id) => {
+    const itemRef = firebase.database().ref(`/items/${id}`);
+    itemRef.remove();
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <p>Hello World</p>
+        <form onSubmit={handleSubmit}>
+          <input
+            name="name"
+            placeholder="Name"
+            value={name}
+            onChange={handleName}
+          />
+          <input
+            name="color"
+            placeholder="Favorite Color"
+            value={color}
+            onChange={handleColor}
+          />
+          <button type="submit">Add Item</button>
+        </form>
+        <div>
+          {items !== undefined ? (
+            items.length > 0 ? (
+              items.map((item) => {
+                return (
+                  <div key={item.id}>
+                    <div>
+                      <p>
+                        {item.name} || {item.color}
+                      </p>
+                      <button
+                        onClick={() => {
+                          removeItem(item.id);
+                        }}
+                      >
+                        Remove
+                      </button>
+                    </div>
+                  </div>
+                );
+              })
+            ) : (
+              <div>No items</div>
+            )
+          ) : (
+            <div>Loading...</div>
+          )}
+        </div>
       </header>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+const config = {
+  apiKey: 'AIzaSyBEpYr78zlaZkLFkUKRAj_mO_ReYJlY_ng',
+  authDomain: 'halfstack-d1196.firebaseapp.com',
+  databaseURL: 'https://halfstack-d1196.firebaseio.com',
+  projectId: 'halfstack-d1196',
+  storageBucket: 'halfstack-d1196.appspot.com',
+  messagingSenderId: '573767894623',
+  appId: '1:573767894623:web:1136689bb4e637d8efbdea',
+};
+firebase.initializeApp(config);
+export default firebase;
